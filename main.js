2f554e657fe6c12345bfe8f9322a306c14494fb0
Antonio Molina <120426089+tonidev404@users.noreply.github.com>
2024-07-26 00:02:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,46 @@ const observador = new IntersectionObserver(Object =>{
 
 
 observador.observe(button);
+
+let input = document.querySelector('.buscador-input');
+let divs = document.querySelectorAll('.div');
+
+
+input.addEventListener('keyup', e=>{
+    let value = input.value;
+    console.log()
+    divs.forEach(e =>{
+        let valor = e.title;
+        if(value == ""){
+            e.style.display = "Block";
+        }
+        else if(valor.toLowerCase().includes(value.toLowerCase())){
+            e.style.display = "block";
+            
+        }
+        else{
+            e.style.display = "none";
+        }
+    
+    });
+});
+
+let lupa = document.querySelector('.lupa');
+let cancelar = document.querySelector('.cancelar')
+
+window.onload = ()=>{
+    cancelar.style.display = "none";
+}
+
+input.addEventListener('focus', () =>{
+    lupa.style.display = "None";
+    cancelar.style.display = "block";
+})
+input.addEventListener('focusout', () =>{
+    lupa.style.display = "block";
+    cancelar.style.display = "none";
+
+})
+cancelar.addEventListener('click', ()=>{
+    alert('Achooo')
+})
